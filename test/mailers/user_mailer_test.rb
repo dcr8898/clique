98fd8a3582c2fc6e064cab31fbcb284d868e721b
Damian C. Rossney <dcr8898@users.noreply.github.com>
2015-06-14 05:30:28 +0000

    Add password resets

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:michael)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "My Clíque Account Activation", mail.subject
+    assert_equal "My Clíque account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
     assert_match user.name, mail.body.encoded
@@ -13,12 +13,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI::escape(user.email), mail.body.encoded
   end
 
-  # test "password_reset" do
-  #   mail = UserMailer.password_reset
-  #   assert_equal "Password reset", mail.subject
-  #   assert_equal ["to@example.org"], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "My Clíque password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
+  end
 
 end
